f0b67e508bba238a69ea18a6281dbc03dc329932
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-02 23:18:18 +0600

    g

--- array.js
+++ array.js
@@ -1,6 +1,24 @@
-function sumOfNumber(numbers) {
-  console.log(numbers)
+// function sumOfNumber(numbers) {
+//   console.log(numbers)
+// }
+// const numbs = [54, 62, 12, 6];
+// const sum = sumOfNumber(num);
+// console.log('Sum of number is', sum);
+
+//********* */
+function evenNumbersOnly(numbers) {
+  const evens = [];
+  for (const number of numbers) {
+    if (number % 2 === 0){
+      console.log(number);
+      evens.push(number);
+      
+    }
+  }
+  return evens;
 }
-const numbs = [54, 62, 12, 6];
-const sum = sumOfNumber(num);
-console.log('Sum of number is', sum);
+
+
+const numbers = [5, 6, 91, 24, 6];
+const evens = evenNumbersOnly(numbers);
+console.log('even numbers are', evens);
