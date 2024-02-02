e569b5f6b0f84e6a023e169f6e49967c0deca943
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-02 20:09:50 +0600

    fFf

---
+++ fun.js
@@ -0,0 +1,12 @@
+const price = 45;
+
+//function declare
+function fanOfffKor(){
+  console.log('bosa theka uthe dara');
+  console.log('Walk towards the switch');
+  console.log('Click the switch to off of the ');
+
+}
+
+//Call the function
+fanOfffKor();
---
+++ func.js
@@ -0,0 +1,10 @@
+function square (number) {
+  console.log('Value of the number partameter');
+  const borgo = number * number;
+  console.log('square of the given number is:',borgo);
+}
+square(4);
+console.log('-------------')
+square(50);
+square(5);
+square(599);
